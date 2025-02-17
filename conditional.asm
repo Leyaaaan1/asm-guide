800914833c8f058f2407448efc9fd8082e50ead2
leyaaan1 <paninsorolean@gmail.com>
2025-02-18 07:16:44 +0800

    add conditional 2 loop

--- conditional.asm
+++ conditional.asm
@@ -10,48 +10,37 @@ includelib \masm32\lib\kernel32.lib
 includelib \masm32\lib\masm32.lib
 
 .data
-
     message db "Number is positive", 13, 10, 0
     message2 db "Number is negative", 13, 10, 0
     number db 10 dup(?)
-    msg db "Positive or Negative: ", 0
+    msg db "Enter a number: ", 0
     num db 2 dup(?)
     newLine db 13, 10, 0
 
-
 .code
-    start:
-        main_loop:
+start:
+
         invoke ClearScreen
+        invoke StdOut, addr msg
+        invoke StdIn, addr number, 10
+        invoke StdOut, addr number
+
+    main_loop:
+        invoke StdOut, addr newLine
         invoke StdOut, addr message
-        invoke StdOut, addr msg
-        invoke StdIn, addr number, 10
 
-        invoke ClearScreen
-        invoke StdOut, addr number
+    second_loop:
         invoke StdOut, addr newLine
-
-        second_loop:
-        invoke ClearScreen
         invoke StdOut, addr message2
-        invoke StdOut, addr msg
-        invoke StdIn, addr number, 10
-
-        invoke ClearScreen
-        invoke StdOut, addr number
-        invoke StdOut, addr newLine
-
-        
-        invoke ClearScreen
-        invoke ExitProcess, 0
-
-        mov al, num
+    
+        mov al, number
         cmp al, '1'
         je main_loop
         cmp al, '2'
         je second_loop
 
-        invoke ClearScreen
         invoke ExitProcess, 0
         
-    end start
+
+
+end start
